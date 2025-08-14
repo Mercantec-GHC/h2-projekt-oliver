9aababf72a2c586ad527b91e0db06decdce5b571
OliverAgB <oliveragervig@gmail.com>
2025-08-14 06:37:49 +0200

    Users - Common

---
+++ API/DBContext/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DomainModels;
+
+namespace API.DBContext;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options)
+        : base(options)
+    {
+    }
+        public DbSet<User> Users { get; set; } = null!;
+    
+}
---
+++ DomainModels/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels;
+
+public class Common
+{
+    [Key]
+    public int Id { get; set; } // Kan erstattes med "int Id"
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
